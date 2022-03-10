31d10216ec4a1f1b15a05d250e272af27e69e8b9
Abdur Rahman Argon <shehab7331@gamil.com>
2022-03-10 22:27:29 +0600

    user image solved

--- js/app.js
+++ js/app.js
@@ -55,6 +55,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post.userImage);
   const image = post.image;
   const div = document.createElement("article");
   div.classList.add("post");
@@ -66,7 +67,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${post.userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
@@ -145,6 +146,7 @@ const showPosts = (posts) => {
   productsContainer.innerHTML = "";
 
   posts.forEach((post) => {
+    // console.log(post);
     const div = createPost(post);
     productsContainer.appendChild(div);
   });
